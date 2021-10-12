5e42580c84fb98b1837b3424b96dda580be1e032
adogarc <adonaye84@gmail.com>
2021-10-12 17:29:56 -0600

    primer commit creacion de pantallas

---
+++ consultorio/src/screens/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text,Component} from 'react-native';
+
+export default class login extends Component{
+
+    render(){
+        return(
+            <View>
+                <View>
+                    <Text>Registro de Cliente</Text>
+                </View>
+            </View>
+        );
+    }
+}
---
+++ consultorio/src/screens/navigation_cliente.js
@@ -0,0 +1,7 @@
+import React from "react";
+import {createBottonTabNavigator} from "@react-navigation/bottom-tabs";
+import Login from './login';
+import Registro from './registro_cliente';
+import CreacionCitasClientes from './registro_cliente';
+import HistorialCliente from './historial_cliente';
+import InfoMedicos from './info_medicos'
