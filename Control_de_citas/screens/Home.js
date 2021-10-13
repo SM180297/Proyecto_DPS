5edaa7a786a56cc6bfc1d45bf192c687c0709a71
C-Cornejo <castillo.cornejo94@gmail.com>
2021-10-13 05:39:08 -0600

    Estilos y contenido de la pantalla home

--- Control_de_citas/screens/Home.js
+++ Control_de_citas/screens/Home.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import { Button,View } from 'react-native';
+import { Button,View,StyleSheet,Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import colores from '../utils/colores';
 
 
 function Home({ navigation }) {
 
     
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          onPress={() => navigation.navigate('Citas')}
-          title="Ir a citas"
-        />
-        <Button
-          onPress={() => navigation.navigate('RegistroCita')}
-          title="Ir a nueva cita"
-        />
-        <Button
-          onPress={() => navigation.navigate('Medico')}
-          title="Ir a medicos"
-        />
+      <View style={styles.contenedor}>
+        <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Citas')}>
+          <View><Text style={styles.etiquetaBtn}>Crear citas</Text></View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Citas')}>
+          <View><Text style={styles.etiquetaBtn}>Ver citas</Text></View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('Citas')}>
+          <View><Text style={styles.etiquetaBtn}>Información de especialistas</Text></View>
+        </TouchableOpacity>
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+    contenedor:
+    {
+      flex: 1, alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: colores.BG_COLOR
+    },
+    btn:
+    {
+      //backgroundColor: colores.DETAILS_COLOR,
+      // width:75,
+      // height:75,
+      padding:15    
+    },
+    etiquetaBtn:{
+      color: colores.AUX_COLOR,
+      fontSize:20
+    }
+  });
 
   export default Home;
